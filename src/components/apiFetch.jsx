49dae94c8e923747e6b330488674c5e61b604a6e
drakeborowski57@gmail.com <drakeborowski57@gmail.com>
2025-11-30 22:24:35 -0500

    changes to newItems.jsx added dynamic photos avatars price and likes along with a skeleton loading state and countdown timers where they can be applied if they arent null along with a carousel

---
+++ src/components/apiFetch.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        console.log("API Response:", response.data);
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+    
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
