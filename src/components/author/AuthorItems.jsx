283b54f5583039e9b82922c50bc9b2975bf74e93
drakeborowski57@gmail.com <drakeborowski57@gmail.com>
2025-12-02 02:21:40 -0500

    added dynamic data and skeleton loading states to author.jsx and authoritems.jsx by hitting the api along with a follow button that increments and decrements and making sure every profile picture links to the right profile

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -2,18 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import useFetch from "../UI/apiFetch";
+import Skeleton from "../UI/Skeleton";
 
-const AuthorItems = () => {
+const AuthorItems = ({ authorId, authorImage }) => {
+
+  const { data: nfts, loading } = useFetch(
+    authorId ? `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}` : null
+  );
+
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
+          {loading ? (
+            new Array(8).fill(0).map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Skeleton width="50px" height="50px" borderRadius="50%" />
+                  </div>
+                  <div className="nft__item_wrap">
+                    <Skeleton width="100%" height="300px" borderRadius="8px" />
+                  </div>
+                  <div className="nft__item_info">
+                    <Skeleton width="80%" height="20px" borderRadius="4px" />
+                    <Skeleton width="60%" height="16px" borderRadius="4px" />
+                    <Skeleton width="40%" height="16px" borderRadius="4px" />
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : nfts?.nftCollection ? (
+            nfts.nftCollection.map((nft, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                  <Link to={`/author/${authorId}`}>
+                    <img className="lazy" src={authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -35,27 +62,32 @@ const AuthorItems = () => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${nft.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={nft.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${nft.nftId}`}>
+                    <h4>{nft.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{nft.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{nft.likes}</span>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
+            ))
+          ) : (
+            <div className="col-md-12">
+              <p>No items found</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
